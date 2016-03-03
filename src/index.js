d137ed6cae8e507367d180bca8945726b22c5a04
sorrycc <sorrycc@gmail.com>
2016-03-03 12:28:51 +0800

    upgrade dora to 0.3

--- src/index.js
+++ src/index.js
@@ -13,23 +13,25 @@ export default {
     this.set('__server_listen_log', false);
   },
   'middleware.before'() {
-    const { log, query } = this;
-    log.debug(`query: ${JSON.stringify(query)}`);
-    const port = query && query.port || 8989;
-    const proxyServer = new ProxyServer({
-      type: 'http',
-      port,
-      hostname: 'localhost',
-      rule: getRule(this),
-      autoTrust: true,
-    });
-    proxyServer.on('finish', (err) => {
-      if (err) {
-        log.error(err);
-      } else {
-        log.info(`listened on ${port}`);
-      }
-      this.callback();
+    return new Promise(resolve => {
+      const { log, query } = this;
+      log.debug(`query: ${JSON.stringify(query)}`);
+      const port = query && query.port || 8989;
+      const proxyServer = new ProxyServer({
+        type: 'http',
+        port,
+        hostname: 'localhost',
+        rule: getRule(this),
+        autoTrust: true,
+      });
+      proxyServer.on('finish', (err) => {
+        if (err) {
+          log.error(err);
+        } else {
+          log.info(`listened on ${port}`);
+        }
+        resolve();
+      });
     });
   },
 };
